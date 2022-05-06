4d63b2364129981a126f5c29ad54d1bf73b147b6
Guilleag01 <guilleag01@gmail.com>
2022-05-06 13:58:45 +0200

    Mejorada la calidad imagenes y vale cualquier slid

--- slideshare.py
+++ slideshare.py
@@ -14,8 +14,9 @@ except:
     print("Es necesario instalar el módulo PIL\nLo puedes instalar con \"pip install Pillow\"")
     raise SystemExit(0)
 import pathlib
+import re
 
-def downloadImages():
+def downloadImages(image1):
     path = pathlib.Path(__file__).parent.absolute()
 
     if exists("images"):
@@ -34,21 +35,29 @@ def downloadImages():
         os.mkdir("images")
     
     print("Descargarndo imágenes de SlideShare...")
-    for i in range(1,22):
-        url = "https://image.slidesharecdn.com/plandecontingencia-140605102313-phpapp01/95/plan-de-contingencia-" + str(i) + "-638.jpg?cb=1401963929"
+    urlParts = re.split(r'-1-1024.jpg', image1)
+    i = 1
+    while(True):
+        #url = "https://image.slidesharecdn.com/plandecontingencia-140605102313-phpapp01/95/plan-de-contingencia-" + str(i) + "-1024.jpg?cb=1401963929 1024w"
+        url = urlParts[0] + "-" + str(i) + "-1024.jpg" + urlParts[1]
 
         img_data = requests.get(url).content
+        if (img_data == b''):
+            break
+        
         imagePath = os.path.join(path, "images")
         imagePath = os.path.join(imagePath, "image-" + str(i) + ".jpg")
         with open(imagePath, 'wb') as handler:
             handler.write(img_data)
+        i+=1
+    return i
 
-def makePDF():
+def makePDF(numImagenes):
     print("Creando PDF...")
     path = pathlib.Path(__file__).parent.absolute()
     #imagesJPG = list()
     imagesRGB = list()
-    for i in range(1,20):
+    for i in range(1,numImagenes + 1):
         imagePath = os.path.join(path, "images")
         imagePath = os.path.join(imagePath, "image-" + str(i) + ".jpg")
         image = Image.open(imagePath)
@@ -56,13 +65,22 @@ def makePDF():
         
     pdfPath = os.path.join(path, "images")
     pdfPath = os.path.join(pdfPath, "image-0.jpg")
-    imagesRGB[0].save("result.pdf", save_all=True, append_images=imagesRGB)
+    imagesRGB[0].save("result.pdf", save_all=True, append_images=imagesRGB[1:])
     print("Se ha creado el PDF \"result.pdf\"")
 
 def main():
-    os.path.join(os.path.dirname(__file__))
-    downloadImages()
-    makePDF()
+    # https://www.slideshare.net/ericschmidt/trillion-dollar-coach-book-bill-campbell
+    url = input("Introduce la url del slideshare que deseas descargar: ")
+    text = requests.get(url).text
+    image1 = ""
+    try:
+        image1 = re.findall(r'https://image.slidesharecdn.com[^".]*1024[^"]*"', text)[0][:-1]
+    except:
+        print("Ha habido un error")
+        raise SystemExit(0)
+
+    numIagenes = downloadImages(image1)
+    makePDF(numIagenes - 1)
 
 if __name__ == "__main__":
     main()
